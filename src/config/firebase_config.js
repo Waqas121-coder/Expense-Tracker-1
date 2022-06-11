f148f393f119bbe46aea716597d82722f11233c5
Waqas121-coder <waqasalisiddiqi121@gmail.com>
2022-06-11 11:10:51 +0500

    first commit

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDSMV7cFIyw6uVY3epEppggX4Ulp3L7IUU",
-    authDomain: "expense-tracker-39166.firebaseapp.com",
-    projectId: "expense-tracker-39166",
-    storageBucket: "expense-tracker-39166.appspot.com",
-    messagingSenderId: "560464578289",
-    appId: "1:560464578289:web:3a5f07cce9d52981e98b50",
-    measurementId: "G-56CE8HY1CD"
+    apiKey: "AIzaSyDGGBvGOh7F0zcrHBr4e0HJCE5ywkGp6s4",
+    authDomain: "expense-generater.firebaseapp.com",
+    projectId: "expense-generater",
+    storageBucket: "expense-generater.appspot.com",
+    messagingSenderId: "340535405986",
+    appId: "1:340535405986:web:c7ab39efebd2239e2816fb",
+    measurementId: "G-ENT3963JDG"
 };
 
 firebase.initializeApp(firebaseConfig);
